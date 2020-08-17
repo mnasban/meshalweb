f7e52c1e45a26f8bd50d9aabedc4e7285bf93d29
Meshal <mnasbn@hotmail.com>
2020-08-17 23:36:54 +0300

    add multi

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -7,6 +7,13 @@ import requests
 import os
 from urllib.parse import unquote, parse_qs
 
+import threading
+from socketserver import ThreadingMixIn
+
+class ThreadHTTPServer(ThreadingMixIn, http.server.HTTPServer):
+    "This is an HTTPServer that supports thread-based concurrency."
+    
+
 memory = {}
 
 form = '''<!DOCTYPE html>
@@ -98,6 +105,7 @@ class Shortener(http.server.BaseHTTPRequestHandler):
                 "Couldn't fetch URI '{}'. Sorry!".format(longuri).encode())
 
 if __name__ == '__main__':
-    server_address = ('', int(os.environ.get('PORT', '8000')))
-    httpd = http.server.HTTPServer(server_address, Shortener)
+    port = int(os.environ.get('PORT', 8000))
+    server_address = ('', port)
+    httpd = ThreadHTTPServer(server_address, Shortener)
     httpd.serve_forever()
